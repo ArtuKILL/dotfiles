2c92601bda99d58408452e5d984f4bf0fd6183dd
ArtuKILL <artolecuona15@gmail.com>
2024-01-17 09:07:40 -0400

    add some config

---
+++ cava/colors.py
@@ -0,0 +1,8 @@
+colors = ["FFFFFF", "FFEAEE", "FFD6DD", "FFC1CC",
+          "FFADBA", "FF98A9", "FF8498", "FF6F87"]
+
+
+print(f"gradient_count={len(colors)}")
+
+for i, color in enumerate(colors):
+    print(f"gradient_color_{i+1} = '#{color}'")
